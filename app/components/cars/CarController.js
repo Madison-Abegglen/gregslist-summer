84283cf3f772a5a9b7344dd101e8cc8a28ed36e6
Madison Abegglen <madison.abegglen@gmail.com>
2018-08-14 15:51:06 -0600

    updates

--- app/components/cars/CarController.js
+++ app/components/cars/CarController.js
@@ -1,11 +1,9 @@
 import CarService from "./CarService.js";
 
-let carService = new CarService()
+let carService = new CarService();
 
-function drawCars() {
-  let cars = carService.getCars()
-  let template = ''
-
+function drawCars(cars) {
+  let template = "";
   for (let i = 0; i < cars.length; i++) {
     const car = cars[i];
     template += `
@@ -13,30 +11,42 @@ function drawCars() {
         <p>Make: ${car.make}</p>
         <p>${car.model}</p>
         <p>${car.price}</p>
+        <button onclick="app.controllers.carController.bid('${car._id}', ${
+      car.price
+    })">Bid</button>
         <p>${car.year}</p>
-        <p>${car.color}</p>
+        <p>${car.description}</p>
         <img src="${car.imgUrl}" alt="somethingelse">
+        <button onclick="app.controllers.carController.deleteCar('${
+          car._id
+        }')">Delete car</button>
     </div>
-    `
+    `;
   }
 
-  document.getElementById('cars').innerHTML = template
-
-
+  document.getElementById("cars").innerHTML = template;
 }
 
 export default class CarController {
-
   constructor() {
-    drawCars()
+    carService.getCars(drawCars);
   }
 
   addCar(triggeredEvent) {
     triggeredEvent.preventDefault();
-    let formData = triggeredEvent.target
-    carService.addCar(formData)
-    formData.reset()
-    drawCars()
+    let formData = triggeredEvent.target;
+    carService.addCar(formData, drawCars);
+    formData.reset();
   }
 
+  deleteCar(carId) {
+    carService.deleteCar(carId, drawCars);
+  }
+
+  bid(carId, price) {
+    let update = {
+      price: price + 100
+    };
+    carService.bid(carId, update, drawCars);
+  }
 }
--- app/components/cars/CarService.js
+++ app/components/cars/CarService.js
@@ -1,47 +1,47 @@
-import Car from '../../models/Car.js'
+import Car from "../../models/Car.js";
 
-let cars = []
+const carsApi = axios.create({
+  baseURL: "https://bcw-gregslist.herokuapp.com/api/cars",
+
+  timeout: 3000
+});
 
 export default class CarService {
-  constructor() {
+  constructor() {}
 
+  getCars(draw) {
+    carsApi.get().then(res => {
+      let cars = res.data.data.map(rawCar => {
+        return new Car(rawCar);
+      });
+      draw(cars);
+    });
   }
 
-  getCars() {
-    let carsCopy = []
+  addCar(formData, draw) {
+    let newCar = new Car({
+      make: formData.make.value,
+      model: formData.model.value,
+      year: formData.year.value,
+      price: formData.price.value,
+      description: formData.description.value,
+      imgUrl: formData.imgUrl.value
+    });
 
-    //same as below
-    // for (let i = 0; i < cars.length; i++) {
-    //   const car = cars[i];
-
-    // }
-
-    // this is just saying for each 'car'
-    //  in the whole cars array do whatever is in the code block 
-    cars.forEach(car => {
-      carsCopy.push(new Car(
-        car.make,
-        car.model,
-        car.year,
-        car.price,
-        car.color,
-        car.imgUrl
-      ))
-    })
-    return carsCopy
+    carsApi.post("", newCar).then(res => {
+      this.getCars(draw);
+    });
   }
 
-  addCar(formData) {
-    let newCar = new Car(
-      formData.make.value,
-      formData.model.value,
-      formData.year.value,
-      formData.price.value,
-      formData.color.value,
-      formData.imgUrl.value
-    )
-    cars.push(newCar)
-    console.log(cars)
+  deleteCar(carId, draw) {
+    carsApi.delete(carId).then(res => {
+      this.getCars(draw);
+    });
+  }
 
+  bid(carId, update, draw) {
+    carsApi.put(carId, update).then(res => {
+      this.getCars(draw);
+    });
   }
 }
